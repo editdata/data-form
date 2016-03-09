f198e58fa778cda9c264066be3c161ee2c68cff5
Kevin Neff <kvnneff@gmail.com>
2016-03-09 10:23:49 -0800

    Simplify.

--- header.js
+++ header.js
@@ -1,13 +1,11 @@
 var h = require('virtual-dom/h')
 
 module.exports = function createFormHeader (options) {
-  return function (state) {
-    return h('div.data-form-header', [
-      h('div.data-form-actions', [
-        h('button.data-form-action.data-form-action-close.button', {
-          onclick: options.onclick
-        }, options.closeButton || 'close')
-      ])
+  return h('div.data-form-header', [
+    h('div.data-form-actions', [
+      h('button.data-form-action.data-form-action-close.button', {
+        onclick: options.onclick
+      }, options.closeButton || 'close')
     ])
-  }
+  ])
 }
--- index.js
+++ index.js
@@ -1,92 +1,110 @@
-var h = require('virtual-dom/h')
-var Emitter = require('component-emitter')
-var fields = require('data-fields')
+/*global requestAnimationFrame*/
 var formatter = require('data-format')()
+var h = require('virtual-dom/h')
+var fieldTypes = require('data-fields')
+var vhook = require('virtual-hook')
 var createHeader = require('./header')
 
-/**
-* Create a form from the row of a dataset and its properties
-* @name createDataForm
-* @param {Object} options
-* @param {Boolean} options.header
-* @param {Object} options.header virtual-dom vtree that should be used for the header
-*/
-module.exports = function createDataForm (options) {
-  options = options || {}
-  var form = {}
-  Emitter(form)
+module.exports = Form
 
+function Form (options) {
+  var activeColumnKey = options.activeColumnKey
+  var oninput = options.oninput
+  var onclick = options.onclick
+  var ondestroy = options.ondestroy
+  var onclose = options.onclose
+  var properties = options.properties
+  var row = options.row
+  var columns = row.value
+  var fields = []
   var header
+
   if (typeof options.header === 'object') {
     header = options.header
   } else if (options.header !== false) {
-    header = createHeader({
-      onclick: function () {
-        form.emit('close')
-      }
-    })
+    header = createHeader({ onclick: onClose })
   }
 
-  /**
-  * Create a form from the row of a dataset and its properties
-  * @name form.render
-  * @param {Object} state
-  * @param {Object} state.activeRow
-  */
-  form.render = function form_render (state) {
-    if (!state.activeRow) return
-    var row = state.activeRow
-    var elements = []
-    var columns = row.value
+  function onInput (rowKey, propertyKey) {
+    return function (event) {
+      var inputValue = event.target.value
+      if (oninput) return oninput(event, rowKey, propertyKey, inputValue)
+    }
+  }
 
-    Object.keys(columns).forEach(function (key) {
-      var property = formatter.findProperty(state.properties, key)
-      var value = columns[key]
-      var type = property.type[0]
+  function onClick (rowKey, propertyKey) {
+    return function (event) {
+      if (onclick) return onclick(event, rowKey, propertyKey)
+    }
+  }
 
-      if (type === 'array') { type = 'list' }
-      if (type === 'object') {
-        if (value.type && value.type === 'Feature') {
-          type = 'geojson'
-        } else {
-          type = 'list'
+  function onDestroy (event) {
+    if (ondestroy) return ondestroy(event, row.key)
+  }
+
+  function onClose (event) {
+    if (onclose) return onclose(event)
+  }
+
+  Object.keys(columns).forEach(function (propertyKey) {
+    var property = formatter.findProperty(properties, propertyKey)
+    var value = columns[propertyKey]
+    var rowKey = row.key
+    var type = property.type[0]
+
+    var hooks = {
+      hook: function (node, prop, prev) {
+        if (propertyKey === activeColumnKey) {
+          requestAnimationFrame(function () {
+            node.focus()
+          })
         }
       }
-
-      var fieldOptions = {
-        fieldType: 'input',
-        id: 'data-field-' + key
-      }
-
-      if (type === 'geojson') {
-        fieldOptions.zoom = 12
-        fieldOptions.center = [47.621958, -122.33636]
-      }
-
-      var field = fields[type](fieldOptions)
-      var vtree = field.render(h, { value: value })
-
-      field.on('update', function (e, value) {
-        row.data.value[key] = value
-        form.emit('update', e, row, value, key)
-      })
-
-      var label = h('label.data-form-label', property.name)
-      var wrapper = h('div.data-form-field-wrapper', [label, vtree])
-      elements.push(wrapper)
-    })
-
-    var sections = []
-
-    if (header) {
-      sections.push(header(state))
     }
 
-    sections.push(h('div.data-form-fields', elements))
-    return h('div#data-form-wrapper', [
-      h('div#data-form', sections)
-    ])
-  }
+    var fieldOptions = {
+      h: h,
+      custom: vhook(hooks),
+      id: 'item-property-' + row.key + '-' + propertyKey,
+      attributes: { 'data-key': propertyKey },
+      value: columns[propertyKey],
+      className: 'item-property-value',
+      oninput: onInput(rowKey, propertyKey),
+      onclick: onClick(rowKey, propertyKey),
+      center: [47.621958, -122.33636],
+      zoom: 12
+    }
 
-  return form
+    if (type === 'array') { type = 'list' }
+    if (type === 'object') {
+      if (value.type && value.type === 'Feature') {
+        type = 'geoJSON'
+      } else {
+        type = 'list'
+      }
+    }
+
+    var field = fieldTypes[type](h, fieldOptions)
+
+    var fieldwrapper = h('div.item-property-wrapper', [
+      h('span.item-property-label', property.name),
+      field
+    ])
+
+    fields.push(fieldwrapper)
+  })
+
+  return h('div#item.active', [
+    h('div.item', [
+      header,
+      h('button#destroyRow.small.button-orange', {
+        onclick: onDestroy
+      }, 'destroy row'),
+      h('div.item-properties-wrapper', {
+        attributes: {
+          'data-key': row.key
+        }
+      }, fields)
+    ])
+  ])
 }
